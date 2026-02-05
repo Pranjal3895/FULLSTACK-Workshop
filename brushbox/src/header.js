f3e19dc6dd274ef039468e721e9b236800c60e03
Pranjal3895 <pranjal.3895@gmail.com>
2026-02-05 10:49:07 +0530

    made changes

--- brushbox/src/header.js
+++ brushbox/src/header.js
@@ -1,5 +1,13 @@
-import {navlink} from "react-router-dom";
 const header = () => {
     return (
         <header className="header">
             <navlink to="/"></navlink>
+            <div>
+                <navlink to={"/"}>Home</navlink>
+                <navlink to={"/aboutus"}>About Us</navlink>
+                <navlink to={"/login"}>Login</navlink>
+            </div>
+        </header>
+    );
+}
+export default header;
